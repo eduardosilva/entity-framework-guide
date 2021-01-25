2750f7c5762608571493359cd03dfd414c32333e
eduardosilva <eduardo.lour.silva@gmail.com>
2021-01-25 10:11:40 -0300

    add new tests

---
+++ src/entity-framework-guide-test/IQuerableExtensionsMethod.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_framework_guide_test
+{
+    public static class IQueryableExtensions
+    {
+        public static Mock<DbSet<T>> BuildMockDbSet<T>(this IQueryable<T> source)
+            where T : class
+        {
+            var mock = new Mock<DbSet<T>>();
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Provider)
+                .Returns(source.Provider);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Expression)
+                .Returns(source.Expression);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.ElementType)
+                .Returns(source.ElementType);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.GetEnumerator())
+                .Returns(source.GetEnumerator());
+
+            return mock;
+        }
+    }
+}
---
+++ src/entity-framework-guide-test/Services/EmployeeServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using entity_framework_guide.Core.Entities;
+using entity_framework_guide.Core.Infrastructure.DataAccess;
+using entity_framework_guide.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace entity_framework_guide_test.Services
+{
+	[TestClass]
+	public class EmployeeServiceTest
+	{
+		private DataContext context;
+		private Employee employee;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			employee = new Employee { Id = 1, NationalIDNumber = "11111" };
+
+			var employeeMockDbSet = new List<Employee>()
+			{
+				employee
+			}
+			.AsQueryable().BuildMockDbSet();
+
+			var mockContext = new Mock<DataContext>();
+			mockContext.Setup(c => c.Employees).Returns(employeeMockDbSet.Object);
+
+			context = mockContext.Object;
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void AddExistingEmployee()
+		{
+			var service = new EmployeeService(this.context);
+			service.AddNewEmployee(new NewEmployeeModel { NationalIDNumber = "11111" });
+		}
+
+		[TestMethod]
+		public void AddNewEmployee()
+		{
+			var service = new EmployeeService(this.context);
+			var result = service.AddNewEmployee(new NewEmployeeModel { NationalIDNumber = "2222" });
+
+			Assert.IsNotNull(result);
+		}
+	}
+}
